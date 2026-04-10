a9570b35e4c12a43082999cdd92d055116c8d546
BillionsX Deploy <deploy@billionsx.com>
2026-04-10 10:19:22 +0000

    trigger: force fresh build

--- apps/web/app/billionsx/page.tsx
+++ apps/web/app/billionsx/page.tsx
@@ -3,3 +3,4 @@
 import dynamic from 'next/dynamic';
 const Main = dynamic(() => import('../page').then(m => ({ default: m.BXStandalone })), { ssr: false });
 export default function BillionsXPage() { return <Main />; }
+// v2
